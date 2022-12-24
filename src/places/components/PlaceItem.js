2cdf2d916e111064181dd0f153fb590cbc789721
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-24 21:47:20 +0300

    Added Frontend error Handling

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,30 +1,32 @@
-import React, { Fragment, useContext, useState } from "react";
+import React, { useState, useContext } from 'react';
 
-import Card from "../../shared/components/UIElements/Card";
-import Button from "../../shared/components/FormElements/Button";
-import Modal from "../../shared/components/UIElements/Modal";
-import Map from "../../shared/components/UIElements/Map";
-import { AuthContext } from "../../shared/context/auth-context";
-import "./PlaceItem.css";
+import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
+import Modal from '../../shared/components/UIElements/Modal';
+import Map from '../../shared/components/UIElements/Map';
+import { AuthContext } from '../../shared/context/auth-context';
+import './PlaceItem.css';
 
-const PlaceItem = (props) => {
+const PlaceItem = props => {
   const auth = useContext(AuthContext);
-
   const [showMap, setShowMap] = useState(false);
-
   const [showConfirmModal, setShowConfirmModal] = useState(false);
 
   const openMapHandler = () => setShowMap(true);
 
   const closeMapHandler = () => setShowMap(false);
 
-  const showDeleteWarningHandler = () => setShowConfirmModal(true);
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
 
-  const cancelDeleteHandler = () => setShowConfirmModal(false);
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+  };
 
   const confirmDeleteHandler = () => {
     setShowConfirmModal(false);
-    console.log("DELETTING.....");
+    console.log('DELETING...');
   };
 
   return (
@@ -47,18 +49,18 @@ const PlaceItem = (props) => {
         header="Are you sure?"
         footerClass="place-item__modal-actions"
         footer={
-          <Fragment>
+          <React.Fragment>
             <Button inverse onClick={cancelDeleteHandler}>
-              Cancel
+              CANCEL
             </Button>
             <Button danger onClick={confirmDeleteHandler}>
-              Proceed
+              DELETE
             </Button>
-          </Fragment>
+          </React.Fragment>
         }
       >
         <p>
-          Do you want to proceed and delete this place? Please note that is
+          Do you want to proceed and delete this place? Please note that it
           can't be undone thereafter.
         </p>
       </Modal>
@@ -79,6 +81,7 @@ const PlaceItem = (props) => {
             {auth.isLoggedIn && (
               <Button to={`/places/${props.id}`}>EDIT</Button>
             )}
+
             {auth.isLoggedIn && (
               <Button danger onClick={showDeleteWarningHandler}>
                 DELETE
--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,44 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import Input from "../../shared/components/FormElements/Input";
-import Button from "../../shared/components/FormElements/Button";
-
-import "./PlaceForm.css";
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import {
   VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH,
-} from "../../shared/util/validators";
-import { useForm } from "../../shared/hooks/form-hook";
-import Card from "../../shared/components/UIElements/Card";
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './PlaceForm.css';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
   {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    id: 'p2',
+    title: 'Emp. State Building',
+    description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
 const UpdatePlace = () => {
@@ -48,29 +47,31 @@ const UpdatePlace = () => {
   const [formState, inputHandler, setFormData] = useForm(
     {
       title: {
-        value: "",
-        isValid: true,
+        value: '',
+        isValid: false
       },
       description: {
-        value: "",
-        isValid: true,
-      },
+        value: '',
+        isValid: false
+      }
     },
     false
   );
-  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
   useEffect(() => {
     if (identifiedPlace) {
       setFormData(
         {
           title: {
             value: identifiedPlace.title,
-            isValid: true,
+            isValid: true
           },
           description: {
             value: identifiedPlace.description,
-            isValid: true,
-          },
+            isValid: true
+          }
         },
         true
       );
@@ -78,15 +79,16 @@ const UpdatePlace = () => {
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
-  const placeUpdateSubmitHandler = (e) => {
-    e.preventDefault();
+  const placeUpdateSubmitHandler = event => {
+    event.preventDefault();
     console.log(formState.inputs);
   };
+
   if (!identifiedPlace) {
     return (
       <div className="center">
         <Card>
-        <h2>Could not find place!</h2>
+          <h2>Could not find place!</h2>
         </Card>
       </div>
     );
@@ -99,6 +101,7 @@ const UpdatePlace = () => {
       </div>
     );
   }
+
   return (
     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
@@ -110,7 +113,7 @@ const UpdatePlace = () => {
         errorText="Please enter a valid title."
         onInput={inputHandler}
         initialValue={formState.inputs.title.value}
-        initialIsValid={formState.inputs.title.isValid}
+        initialValid={formState.inputs.title.isValid}
       />
       <Input
         id="description"
@@ -120,7 +123,7 @@ const UpdatePlace = () => {
         errorText="Please enter a valid description (min. 5 characters)."
         onInput={inputHandler}
         initialValue={formState.inputs.description.value}
-        initialIsValid={formState.inputs.description.isValid}
+        initialValid={formState.inputs.description.isValid}
       />
       <Button type="submit" disabled={!formState.isValid}>
         UPDATE PLACE
