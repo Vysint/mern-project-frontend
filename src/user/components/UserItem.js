fd84a181bc212e33b415b1d5be1412518502f617
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-26 20:58:43 +0300

    Fixed an image upload url

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -12,7 +12,7 @@ const UserItem = (props) => {
         <Link to={`/${props.id}/places`}>
           <div className="user-item__image">
             <Avatar
-              image={`http:localhost:5001/${props.image}`}
+              image={`http://localhost:5001/${props.image}`}
               alt={props.name}
             />
           </div>
