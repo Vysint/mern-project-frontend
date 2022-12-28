13128e85ff36dda3c82b5af13d3cee0054716861
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-29 01:29:45 +0300

    Added code splitting in React

---
+++ build/precache-manifest.6aeaeaa8322d4e42b7b7949c215d979d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ee4bb7934271ddbfc69faded45d33f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbcbf258085fbef5639d",
+    "url": "/static/css/3.2b2a4eea.chunk.css"
+  },
+  {
+    "revision": "03f206e9065db9d7336d",
+    "url": "/static/css/4.69bed00c.chunk.css"
+  },
+  {
+    "revision": "e6e553069c9dceee233c",
+    "url": "/static/css/5.1dcbddb4.chunk.css"
+  },
+  {
+    "revision": "f8194c85665b6477bd98",
+    "url": "/static/css/6.1e5f2690.chunk.css"
+  },
+  {
+    "revision": "35fdc0de684e02360d25",
+    "url": "/static/css/7.39533c54.chunk.css"
+  },
+  {
+    "revision": "33ec742df2c6512f27ba",
+    "url": "/static/css/main.65269f5a.chunk.css"
+  },
+  {
+    "revision": "d2605d9926108c0beda4",
+    "url": "/static/js/2.400b6845.chunk.js"
+  },
+  {
+    "revision": "dbcbf258085fbef5639d",
+    "url": "/static/js/3.d40a986b.chunk.js"
+  },
+  {
+    "revision": "03f206e9065db9d7336d",
+    "url": "/static/js/4.c1d22833.chunk.js"
+  },
+  {
+    "revision": "e6e553069c9dceee233c",
+    "url": "/static/js/5.7562b8db.chunk.js"
+  },
+  {
+    "revision": "f8194c85665b6477bd98",
+    "url": "/static/js/6.a809548d.chunk.js"
+  },
+  {
+    "revision": "35fdc0de684e02360d25",
+    "url": "/static/js/7.c8c0b388.chunk.js"
+  },
+  {
+    "revision": "33ec742df2c6512f27ba",
+    "url": "/static/js/main.3b5bcdf4.chunk.js"
+  },
+  {
+    "revision": "cb091ccc550bda50bb6f",
+    "url": "/static/js/runtime-main.96a0a1bf.js"
+  }
+]);
